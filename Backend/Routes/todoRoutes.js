c47743923a40c85f025a8a366cda58c7fbabec6b
Anshul Pawar <anshulpawar100@gmail.com>
2024-01-26 20:53:38 +0530

    second changes

--- Backend/Routes/todoRoutes.js
+++ Backend/Routes/todoRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const controller = require("./todoController");
+const controller = require("../Controller/todoController");
 
 const router = express.Router();
 
