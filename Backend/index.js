da685c8c9861f03f3b0e13b42b48459be234025d
Anshul Pawar <anshulpawar100@gmail.com>
2024-01-26 21:12:52 +0530

    update noteReducer

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const todoRoutes = require("./todoRoutes");
+const todoRoutes = require("./Routes/todoRoutes");
 const cors = require("cors");
 const bodyParser = require('body-parser');
 
--- Frontend/src/components/NoteList/NoteList.js
+++ Frontend/src/components/NoteList/NoteList.js
@@ -6,21 +6,21 @@ import { noteSelector } from "../../redux/reducers/noteReducer";
 import styles from "./NoteList.module.css";
 
 function NoteList() {
-    const notes=useSelector(noteSelector);
-    const dispatch=useDispatch();
+  const notes = useSelector(noteSelector);
+  const dispatch = useDispatch();
 
   return (
     <div className="container">
-    <ul>
-      {notes.map((note,index) => (
-        <li className={styles.item}>
-            <p>{note.createdOn.toLocaleDateString()}</p>
+      <ul>
+        {notes.map((note, index) => (
+          <li className={styles.item}>
+            <p>{note.createdOn}</p>
             <p className={styles.notecontent}>{note.text}</p>
-            <button className={"btn btn-danger "+styles.button}
-            onClick={()=> dispatch(actions.delete(index))}>Delete</button>
-            </li>
-      ))}
-    </ul>
+            <button className={"btn btn-danger " + styles.button}
+              onClick={() => dispatch(actions.delete(index))}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
